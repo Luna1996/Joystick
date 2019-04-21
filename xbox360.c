4fdd7ae6b0a4210b564064ca7699e1cae6ad42ed
l+l l +K <2320909844@qq.com>
2019-04-21 08:50:56 -0500

    Update xbox360.c

--- xbox360.c
+++ xbox360.c
@@ -12,7 +12,8 @@ typedef struct {
 
 int main(int argc, char **argv) {
   const char *device;
-  js_event e;
+  // js_event e;
+  char e[8];
   i32 js, es;
 
   if (argc > 1)
@@ -21,10 +22,9 @@ int main(int argc, char **argv) {
     device = "/dev/input/js0";
 
   js = open(device, O_RDONLY);
-  printf("js:%d\n", js);
-  es = sizeof(e);
-  read(js, &e, es);
-  printf("time:%u type:%u name:%u value:%d\n", e.time, e.type, e.name, e.value);
+  int s = read(js, &e, 8);
+  for (int i = 0; i < 8; i++) printf("%04x ", e[i]);
+  printf("size:%d\n", s);
   close(js);
   return 0;
 }
