9c0d7be929d487c54ee9e66104e970ae170ddb6f
l+l l +K <2320909844@qq.com>
2019-04-21 09:11:02 -0500

    Update xbox360.c

--- xbox360.c
+++ xbox360.c
@@ -4,6 +4,7 @@
 #include <sys/stat.h>
 #include <sys/types.h>
 #include <unistd.h>
+#include "types.h"
 
 int main(int argc, char **argv) {
   int fd;
@@ -15,10 +16,12 @@ int main(int argc, char **argv) {
   struct input_event ev;
 
   int s;
+  u8 e[16];
   while (1) {
-    s = read(fd, &ev, sizeof(struct input_event));
-    printf("size:%d", s);
-    printf("time:%d type:%d code:%d value:%d\n", ev.time, ev.type, ev.code,
-           ev.value);
+    s = read(fd, e, 16);
+    for (int i = 0; i < 16; i++) {
+      printf("%02x ", e[i]);
+    }
+    printf("size:%d\n", s);
   }
 }
