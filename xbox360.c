dd6bf0e7238db761048f7a5ba690d6490d69b015
l+l l +K <2320909844@qq.com>
2019-04-21 09:17:34 -0500

    Update xbox360.c

--- xbox360.c
+++ xbox360.c
@@ -6,6 +6,9 @@
 #include <unistd.h>
 #include "types.h"
 
+typedef struct {
+} js_event;
+
 int main(int argc, char **argv) {
   int fd;
   if (argc < 2) {
@@ -16,10 +19,10 @@ int main(int argc, char **argv) {
   struct input_event ev;
 
   int s;
-  u8 e[16];
+  u8 e[100];
   while (1) {
-    s = read(fd, e, 16);
-    for (int i = 0; i < 16; i++) {
+    s = read(fd, e, 100);
+    for (int i = 0; i < s; i++) {
       printf("%02x ", e[i]);
     }
     printf("size:%d\n", s);
