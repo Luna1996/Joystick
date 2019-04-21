b707a818bd77f0897ce7de92ea3d6f07098304cd
l+l l +K <2320909844@qq.com>
2019-04-21 09:45:33 -0500

    Update xbox360.c

--- xbox360.c
+++ xbox360.c
@@ -1,15 +1,13 @@
 #include <fcntl.h>
-#include <linux/input.h>
 #include <stdio.h>
-#include <sys/stat.h>
-#include <sys/types.h>
 #include <unistd.h>
 #include "types.h"
 
 typedef struct {
-  u64 time;
+  u32 sec;
+  u32 usec;
   u16 type;
-  u16 name;
+  u16 code;
   i32 value;
 } js_event;
 
@@ -20,12 +18,12 @@ int main(int argc, char **argv) {
     return 1;
   }
   fd = open(argv[1], O_RDONLY);
-  input_event ev;
+  js_event ev;
 
   while (1) {
     s = read(fd, &ev, 16);
-    printf("time:%016x type:%04x code:%04x value:%08x\n", ev.time, ev.type,
-           ev.code, ev.value);
+    printf("sec:%08x usec:%08x type:%04x code:%04x value:%08x\n", ev.sec,
+           ev.usec, ev.type, ev.code, ev.value);
     // for (int i = 0; i < s; i++) printf("%02x ", ev[i]);
     // printf("\n");
   }
